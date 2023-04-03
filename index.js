578edbb2c491c0a8ed60e38528d299c039955365
pankaj1707k <76695979+pankaj1707k@users.noreply.github.com>
2023-04-03 23:20:32 +0530

    feat(index.js): set up express server

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const server = require("http").createServer();
-const io = require("socket.io")(server);
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
-io.on("connection", (client) => {
-  client.on("event", (data) => {
-    /* … */
-  });
-  client.on("disconnect", () => {
-    /* … */
-  });
+// Update env vars from `.env` file
+dotenv.config();
+
+const app = express();
+
+// connect to mongodb
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// attach api routes and handlers here
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 8000;
+const server = app.listen(PORT, () =>
+  console.log(`Server listening on port ${PORT}`)
+);
+
+process.on("unhandledRejection", (err, _) => {
+  console.log(`Logged error: ${err.message}`);
+  server.close(() => process.exit(1));
 });
-server.listen(3000);
